c0cd85e1f01bfb8bb5b619924510c3bfc4dbbe93
김선옥 <sictami1012@gmailc.om>
2019-09-05 09:09:41 +0900

    foods.fruits 활용

--- 패키지/foods/drinks/__init__.py
+++ 패키지/foods/drinks/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['cola','mill','water']
+__all__ = ['cola','milk','water']
---
+++ 패키지/main_fruits.py
@@ -0,0 +1,6 @@
+# from foods.fruits import grape,peach,watermelon
+from foods.fruits import *
+
+grape.eat()
+peach.eat()
+watermelon.eat()
